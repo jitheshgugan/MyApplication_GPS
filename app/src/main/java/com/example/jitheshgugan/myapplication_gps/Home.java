755732ee638fd358156a2b2176da3af587da9bac
JitheshGugan <sreeramjithesh23@gmail.com>
2018-06-08 21:52:02 +0530

    With accelerometer data obtaining system

--- app/src/main/java/com/example/jitheshgugan/myapplication_gps/Home.java
+++ app/src/main/java/com/example/jitheshgugan/myapplication_gps/Home.java
@@ -6,16 +6,21 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
+import android.view.Surface;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -33,6 +38,10 @@ public class Home extends AppCompatActivity {
     Context cont;
     TextView view1;
 
+    public Display mDisplay;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -40,9 +49,47 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         cont = getApplicationContext();
-
         view1 = (TextView) findViewById(R.id.view1);
         locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener sensorEventListener = new SensorEventListener() {
+
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+
+            public void onSensorChanged(SensorEvent event) {
+                //Right in here is where you put code to read the current sensor values and
+                //update any views you might have that are displaying the sensor information
+                //You'd get accelerometer values like this:
+                Log.i("Sensor", event.sensor.getName() + " value: " + event.values[0]);
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    float mSensorX = 0, mSensorY = 0;
+                    switch (mDisplay.getRotation()) {
+                        case Surface.ROTATION_0:
+                            mSensorX = event.values[0];
+                            mSensorY = event.values[1];
+                            break;
+                        case Surface.ROTATION_90:
+                            mSensorX = -event.values[1];
+                            mSensorY = event.values[0];
+                            break;
+                        case Surface.ROTATION_180:
+                            mSensorX = -event.values[0];
+                            mSensorY = -event.values[1];
+                            break;
+                        case Surface.ROTATION_270:
+                            mSensorX = event.values[1];
+                            mSensorY = -event.values[0];
+                    }
+                    view1.setText("X: " + event.values[0] + "\nY: " + event.values[1]);
+                }
+            }
+        };
+        mSensorManager.registerListener(sensorEventListener,
+                mAccelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
 
         Button functionally;
             functionally = (Button)findViewById(R.id.button1);
@@ -56,43 +103,38 @@ public class Home extends AppCompatActivity {
             });
 
             }
-
-
-
-
-
-
-
-
-
-
         //li = new speed();
 
-        protected void functionally() {
+    protected void functionally() {
 
-            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10, 10, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location loc) {
-                    Float thespeed = loc.getSpeed();
-                    Toast.makeText(cont, String.valueOf(thespeed), Toast.LENGTH_LONG).show();
-                    view1.setText(thespeed.toString());
-                }
+        checkCallingOrSelfPermission();
+        locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10, 10, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location loc) {
+                Float thespeed = loc.getSpeed();
+                Toast.makeText(cont, String.valueOf(thespeed), Toast.LENGTH_LONG).show();
+                view1.setText(thespeed.toString());
+            }
 
-                @Override
-                public void onProviderDisabled(String arg0) {
-                }
+            @Override
+            public void onProviderDisabled(String arg0) {
+            }
 
-                @Override
-                public void onProviderEnabled(String arg0) {
-                }
+            @Override
+            public void onProviderEnabled(String arg0) {
+            }
 
-                @Override
-                public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-                }
-            });
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+            }
+        });
+    }
+
+
+    private void checkCallingOrSelfPermission() {
 
         if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
+
 
             if (ContextCompat.checkSelfPermission(cont,
                     Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -136,7 +178,11 @@ public class Home extends AppCompatActivity {
                 }
             });
         }
-    };
+    }
+
+
+
+
 
 
 
