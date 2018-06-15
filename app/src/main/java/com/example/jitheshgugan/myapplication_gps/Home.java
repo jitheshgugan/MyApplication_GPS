09074733e78ffcb4cffe79f85cd54776a36b3adc
JitheshGugan <sreeramjithesh23@gmail.com>
2018-06-15 11:30:17 +0530

    GPS Location permission 15/06/2018

--- app/src/main/java/com/example/jitheshgugan/myapplication_gps/Home.java
+++ app/src/main/java/com/example/jitheshgugan/myapplication_gps/Home.java
@@ -2,7 +2,6 @@ package com.example.jitheshgugan.myapplication_gps;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -11,29 +10,24 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
 public class Home extends AppCompatActivity {
 
     private LocationManager locManager;
     private LocationListener li;
-    public final int MY_PERMISSIONS_REQUEST_LOCATION=2;
+    public final int MY_PERMISSIONS_REQUEST_LOCATION = 2;
     Context cont;
     TextView view1;
     public String mDisplay;
@@ -159,155 +153,45 @@ public class Home extends AppCompatActivity {
         };
 
 
-        mSensorManager.registerListener(sensorEventListener,
-                mAccelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(sensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-
-            }
-        //li = new speed();
-
-    protected void functionally() {
-
-        checkCallingOrSelfPermission();
-        locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10, 10, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location loc) {
-                Float thespeed = loc.getSpeed();
-                Toast.makeText(cont, String.valueOf(thespeed), Toast.LENGTH_LONG).show();
-                view1.setText(thespeed.toString());
-            }
-
-            @Override
-            public void onProviderDisabled(String arg0) {
-            }
-
-            @Override
-            public void onProviderEnabled(String arg0) {
-            }
-
-            @Override
-            public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-            }
-        });
-    }
-
-
-    private void checkCallingOrSelfPermission() {
-
-        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-
-            if (ContextCompat.checkSelfPermission(cont,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) cont,
-                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
-
-
-                } else {
-                    // No explanation needed; request the permission
-                    ActivityCompat.requestPermissions((Activity) cont,
-                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                            MY_PERMISSIONS_REQUEST_LOCATION);
-
-                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                    // app-defined int constant. The callback method gets the
-                    // result of the request.
-                }
-
-
-            }
-
-            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location loc) {
-                    Float thespeed = loc.getSpeed();
-                    Toast.makeText(cont, String.valueOf(thespeed), Toast.LENGTH_LONG).show();
-                    view1.setText(thespeed.toString());
-                }
-
-                @Override
-                public void onProviderDisabled(String arg0) {
-                }
-
-                @Override
-                public void onProviderEnabled(String arg0) {
-                }
-
-                @Override
-                public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-                }
-            });
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
-        return result == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestPermission() {
-
-        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION:
-                if (grantResults.length > 0) {
+}
 
-                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
 
-                    if (locationAccepted)
-                        Toast.makeText( cont,"Permission Granted, Now you can access location data and camera.", Toast.LENGTH_LONG).show();
-                    else {
-
-                        Toast.makeText( cont,"Permission not Granted, Now you can not access location data and camera.", Toast.LENGTH_LONG).show();
-
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
-                                showMessageOKCancel("You need to allow access to both the permissions",
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                                    requestPermissions(new String[]{ACCESS_FINE_LOCATION},
-                                                            MY_PERMISSIONS_REQUEST_LOCATION);
-                                                }
-                                            }
-                                        });
-                                return;
-                            }
-                        }
-
+    private void checkForLocationPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(LocationListener.this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+                alertBuilder.setCancelable(true);
+                alertBuilder.setTitle("Location permission necessary");
+                alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(onSensorChanged.this,
+                                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                MY_PERMISSIONS_REQUEST_LOCATION);
                     }
-                }
+                });
 
+                AlertDialog alert = alertBuilder.create();
+                alert.show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+                // MY_PERMISSIONS_REQUEST_CAMERA is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
 
-                break;
         }
     }
-
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(cont)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-
 }
